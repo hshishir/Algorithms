5ae7a7a42ec840f8740d33159a68867ddb64dbc1
Hemang Shishir <hshishir@outlook.com>
2017-07-29 23:25:18 -0700

    Algorithms

---
+++ src/QuickUnion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class QuickUnion
+    {
+        private int size;
+        private int[] array;
+
+        public QuickUnion(int l)
+        {
+            size = l;
+            array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = i;
+            }
+        }
+
+        public int Root(int p)
+        {
+            while (array[p] != p)
+            {
+                p = array[p];
+            }
+
+            return p;
+        }
+
+        public void Union(int p, int q)
+        {
+            int proot = Root(p);
+            int qroot = Root(q);
+            array[proot] = qroot;
+        }
+
+        public bool Connected(int p, int q)
+        {
+            var proot = Root(p);
+            var qroot = Root(q);
+            var result = proot == qroot;
+            Console.WriteLine($"{p} root: {proot}");
+            Console.WriteLine($"{q} root: {qroot}");
+            Console.WriteLine($"{p} - {q}: {result}");
+            return result;
+        }
+
+        public void Display()
+        {
+            var str = string.Empty;
+            foreach (var a in array)
+            {
+                str += $"{a} ";
+            }
+
+            Console.WriteLine(str);
+        }
+    }
+
+    public class QuickUnionTest
+    {
+        public static void Execute()
+        {
+            var qu = new QuickUnion(10);
+            qu.Display();
+            qu.Union(4, 3);
+            qu.Union(3, 8);
+            qu.Union(6, 5);
+            qu.Union(9, 4);
+            qu.Union(2, 1);
+            qu.Union(5, 0);
+            qu.Union(7, 2);
+            qu.Union(6, 1);
+            qu.Union(7, 3);
+
+            qu.Display();
+        }
+    }
+}
---
+++ src/StackLinkedList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class StackLinkedList
+    {
+        private Node head;
+        private int count;
+        private class Node
+        {
+            public string item;
+            public Node next;
+
+            public Node(string str)
+            {
+                item = str;
+                next = null;
+            }
+        }
+
+        public void Push(string item)
+        {
+            var oldHead = head;
+            head = new Node(item);
+            head.next = oldHead;
+            count++;
+        }
+
+        public string Pop()
+        {
+            if (head == null)
+            {
+                return string.Empty;
+            }
+
+            var tmp = head;
+            head = head.next;
+            var str = tmp.item;
+            tmp = null;
+            return str;
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+    }
+
+    public class StackLinkedListTest
+    {
+        public static void Execute()
+        {
+            var sl = new StackLinkedList();
+            sl.Push("hemang");
+            sl.Push("is");
+            sl.Push("my");
+            sl.Push("name");
+
+            System.Console.WriteLine(sl.Pop());
+            System.Console.WriteLine(sl.Pop());
+        }
+    }
+}
