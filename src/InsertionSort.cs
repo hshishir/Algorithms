a56e59d077e8e8ef782c28b4aae30026c4f41c6f
Hemang Shishir <hshishir@outlook.com>
2017-08-02 15:27:03 -0700

    Changes

---
+++ src/InsertionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class InsertionSort<T> where T : IComparable<T>
+    {
+        private T[] array;
+
+        public void Sort(T[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (data[j].CompareTo(data[j-1]) < 0)
+                    {
+                        Exchange(data, j, j-1);
+                    }
+                }
+            }
+        }
+
+        private void Exchange(T[] array, int p, int q)
+        {
+            var tmp = array[p];
+            array[p] = array[q];
+            array[q] = tmp;
+        }
+    }
+}
